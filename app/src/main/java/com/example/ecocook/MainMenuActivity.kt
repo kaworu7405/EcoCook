a5847392ad4c52dba2e7507e8593c58b22aaa446
jina0 <1226___snow@naver.com>
2021-10-12 16:22:28 +0900

    profile_Photo_Upload 프로필 사진 설정

--- app/src/main/java/com/example/ecocook/MainMenuActivity.kt
+++ app/src/main/java/com/example/ecocook/MainMenuActivity.kt
@@ -48,14 +48,38 @@ class MainMenuActivity: AppCompatActivity() {
 
 
         } else { //user가 null이면 login 화면으로
-            val nextIntent = Intent(this, LoginActivity::class.java)
-            startActivity(nextIntent)
+            startActivity(Intent(this, LoginActivity::class.java))
         }
 
-        TempLogoutButton.setOnClickListener{
-            //Login Activity로 이동
-            FirebaseAuth.getInstance().signOut()
-            startActivity(Intent(this, LoginActivity::class.java))
+        TempLogoutButton.setOnClickListener{ //로그아웃 버튼
+            FirebaseAuth.getInstance().signOut() //로그아웃
+            startActivity(Intent(this, LoginActivity::class.java))//Login Activity로 이동
+        }
+        
+        //DB TEST할 수 있는 버튼!
+        //DB에 저장되어있는 레시피를 꺼내올 수 있음
+        DbTestButton.setOnClickListener{
+            val db = Firebase.firestore
+
+            db.collection("recipes")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        var data=document.getData()?.get("name").toString()+"먹고싶다"
+                        
+                        Toast.makeText(baseContext, data,
+                            Toast.LENGTH_SHORT).show()
+
+                        //Log.d(TAG, "${document.id} => ${document.data}")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    //Log.d(TAG, "Error getting documents: ", exception)
+                }
+        }
+
+        MyPageButton.setOnClickListener{
+            startActivity(Intent(this, MyPageActivity::class.java))
         }
     }
 
