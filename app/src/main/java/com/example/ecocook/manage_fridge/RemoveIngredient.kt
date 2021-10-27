ed126f117647c84514f458b6ef00865431367225
Jaehun.Kim <rlawogns0411@naver.com>
2021-10-28 03:52:29 +0900

    feat : add RemoveIngredient

---
+++ app/src/main/java/com/example/ecocook/manage_fridge/RemoveIngredient.kt
@@ -0,0 +1,12 @@
+package com.example.ecocook.manage_fridge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecocook.R
+
+class RemoveIngredient : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_remove_ingredient)
+    }
+}
