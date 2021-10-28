c4e7004b8f228b65b71d8f603cec3ccdbdca133e
Jaehun.Kim <rlawogns0411@naver.com>
2021-10-29 04:35:27 +0900

    feat : add function

--- app/src/main/java/com/example/ecocook/manage_fridge/MyFridge.kt
+++ app/src/main/java/com/example/ecocook/manage_fridge/MyFridge.kt
@@ -1,6 +1,7 @@
 package com.example.ecocook.manage_fridge
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
@@ -14,23 +15,49 @@ import kotlinx.android.synthetic.main.activity_my_fridge.*
 
 class MyFridge : AppCompatActivity() {
     var ingredient = 0
-
+    var icount=0
+    var lcount=0
     var backKeyPressedTime:Long=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_fridge)
-        println("id")
-        println(tr1.id)
-        val add_btn = findViewById<Button>(R.id.ingredient_add) as Button
         add_btn.setOnClickListener(View.OnClickListener {
-            startActivity(Intent(this, AddIngredient::class.java))
-            finish()
-            /*
+            //startActivity(Intent(this, AddIngredient::class.java))
+            //finish()
+            if(icount==lcount*4){
+                AddTablerow()
+                lcount+=1
+            }
             AddLinear()
             AddIngredient()
-            ingredient+=1
-            */
+            icount+=1
         })
+        remove_btn.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this, RemoveIngredient::class.java))
+            finish()
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        var pref = getSharedPreferences("pref",Context.MODE_PRIVATE)
+        ingredient = pref.getInt("ingredient",0)
+        System.out.println(ingredient)
+        for(i in 0..ingredient-1){
+            if(icount==lcount*4){
+                AddTablerow()
+                lcount+=1
+            }
+            AddLinear()
+            AddIngredient()
+            icount+=1
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        var pref = getSharedPreferences("pref",Context.MODE_PRIVATE)
+        pref.edit().putInt("ingredient",icount).apply()
     }
     override fun onBackPressed() {
         /*if(System.currentTimeMillis()-backKeyPressedTime<2000){
@@ -40,65 +67,73 @@ class MyFridge : AppCompatActivity() {
         startActivity(Intent(this, MainMenuActivity::class.java))
         finish()
     }
+    fun AddTablerow(){        //한 줄 추가
+        val LL= TableRow(this)
+        LL.setBackgroundColor(Color.GREEN)
+        //LL.id=id                      //rse에 있는 아이디사용
+        LL.id=(50000+lcount)
+        table1.addView(LL,TableRow.LayoutParams.MATCH_PARENT, changeDP(110))
+        //(findViewById<LinearLayout>(10000+ingredient).layoutParams as LinearLayout.LayoutParams).weight=1f    weight설정
+
+
+    }
     @SuppressLint("ResourceType")       //이건 뭔지 잘 모르겠다, id=(int값)할 때 뜨는 에러때문에 추가
-    fun AddLinear(){
+    fun AddLinear(){        //한 줄 추가
         //val idArray = resources.obtainTypedArray(R.array.id_group_1)      //res에 있는 아이디사용
         //val id = idArray.getResourceId(ingredient,-1)
         val LL= LinearLayout(this)
         LL.setBackgroundColor(Color.GREEN)
         //LL.id=id                      //rse에 있는 아이디사용
-        LL.id=(10000+ingredient)
+        LL.id=(10000+icount)
+        LL.gravity=Gravity.CENTER
         LL.orientation=LinearLayout.VERTICAL
-        tr1.addView(LL,LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+        findViewById<TableRow>(50000+lcount-1).addView(LL,LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
         //(findViewById<LinearLayout>(10000+ingredient).layoutParams as LinearLayout.LayoutParams).weight=1f    weight설정
-        println(tr1.id)
-        println(tr2.id)
-        println(tr3.id)
+
 
     }
     @SuppressLint("ResourceType")   //이건 뭔지 잘 모르겠다, id=(int값)할 때 뜨는 에러때문에 추가
-    fun AddIngredient(){
+    fun AddIngredient(){    //재료 추가
         //val idArray = resources.obtainTypedArray(R.array.id_group_1)  //rse에 있는 아이디사용
         //val id = idArray.getResourceId(ingredient,-1)
 
         val img= ImageView(this)
         img.setImageResource(R.drawable.testaa)
         img.scaleType=ImageView.ScaleType.CENTER_CROP
-        img.id=(20000+ingredient)
+        img.id=(20000+icount)
         img.layoutParams = LinearLayout.LayoutParams(
             changeDP(100),
             changeDP(60)
         )
-        //findViewById<LinearLayout>(id).addView(img)
-        findViewById<LinearLayout>(10000+ingredient).addView(img)
+        findViewById<LinearLayout>(10000+icount).addView(img)
 
         val textname = TextView(this)
         textname.text = "감자"
         textname.gravity=Gravity.CENTER
         textname.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
             changeDP(30)
         )
-        textname.id=(30000+ingredient)
+        textname.id=(30000+icount)
         textname.setBackgroundColor(Color.GRAY)
-        //findViewById<LinearLayout>(id).addView(textname)
-        findViewById<LinearLayout>(10000+ingredient).addView(textname)
+        findViewById<LinearLayout>(10000+icount).addView(textname)
 
         val textdate = TextView(this)
         textdate.text = "D-3"
         textdate.gravity=Gravity.CENTER
         textdate.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
             changeDP(20)
         )
         textdate.setBackgroundColor(Color.WHITE)
-        textdate.id=(40000+ingredient)
-        //findViewById<LinearLayout>(id).addView(textdate)
-        findViewById<LinearLayout>(10000+ingredient).addView(textdate)
+        textdate.id=(40000+icount)
+        findViewById<LinearLayout>(10000+icount).addView(textdate)
     }
     private fun changeDP(value : Int) : Int{        //int값을 dp값으로 변환
         var displayMetrics = resources.displayMetrics
         var dp = Math.round(value * displayMetrics.density)
         return dp
     }
+
+
 }
--- app/src/main/java/com/example/ecocook/manage_fridge/RemoveIngredient.kt
+++ app/src/main/java/com/example/ecocook/manage_fridge/RemoveIngredient.kt
@@ -1,12 +1,135 @@
 package com.example.ecocook.manage_fridge
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.*
 import com.example.ecocook.R
+import kotlinx.android.synthetic.main.activity_my_fridge.*
+import kotlinx.android.synthetic.main.activity_remove_ingredient.*
 
 class RemoveIngredient : AppCompatActivity() {
+    var ingredient = 0
+    var icount = 0
+    var lcount = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_remove_ingredient)
+        select_btn.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this, MyFridge::class.java))
+            finish()
+        })
+        select_remove_btn.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this, MyFridge::class.java))
+            finish()
+        })
     }
+    override fun onBackPressed() {
+        startActivity(Intent(this, MyFridge::class.java))
+        finish()
+    }
+    override fun onStart() {
+        super.onStart()
+        var pref = getSharedPreferences("pref", Context.MODE_PRIVATE)
+        ingredient = pref.getInt("ingredient",0)
+        for(i in 0..ingredient-1){
+            if(icount==lcount*4){
+                AddTablerow()
+                lcount+=1
+            }
+            AddLinear()
+            AddIngredient()
+            icount+=1
+        }
+    }
+    fun AddTablerow(){        //한 줄 추가
+        val LL= TableRow(this)
+        LL.setBackgroundColor(Color.GREEN)
+        LL.id=(60000+lcount)
+        table2.addView(LL, TableRow.LayoutParams.MATCH_PARENT, changeDP(110))
+    }
+    @SuppressLint("ResourceType")       //이건 뭔지 잘 모르겠다, id=(int값)할 때 뜨는 에러때문에 추가
+    fun AddLinear(){        //한 줄 추가
+        val LL1= LinearLayout(this)
+        LL1.setBackgroundColor(Color.GREEN)
+        LL1.id=(61000+icount)
+        LL1.gravity= Gravity.CENTER
+        LL1.orientation= LinearLayout.HORIZONTAL
+        findViewById<TableRow>(60000+lcount-1).addView(LL1)
+        (findViewById<LinearLayout>(61000+icount).layoutParams as LinearLayout.LayoutParams).setMargins(changeDP(20),changeDP(5),0,0)
+
+        val LL2= LinearLayout(this)
+        LL2.setBackgroundColor(Color.GREEN)
+        LL2.id=(62000+icount)
+        LL2.gravity= Gravity.CENTER
+        LL2.orientation= LinearLayout.VERTICAL
+        findViewById<LinearLayout>(61000+icount).addView(LL2,
+            LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        val LL3= LinearLayout(this)
+        LL3.setBackgroundColor(Color.GREEN)
+        LL3.id=(63000+icount)
+        LL3.gravity= Gravity.CENTER
+        findViewById<LinearLayout>(61000+icount).addView(LL3,
+            LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT)
+
+    }
+    @SuppressLint("ResourceType")   //이건 뭔지 잘 모르겠다, id=(int값)할 때 뜨는 에러때문에 추가
+    fun AddIngredient(){    //재료 추가
+        val img= ImageView(this)
+        img.setImageResource(R.drawable.testaa)
+        img.scaleType= ImageView.ScaleType.CENTER_CROP
+        img.id=(64000+icount)
+        img.layoutParams = LinearLayout.LayoutParams(
+            changeDP(80),
+            changeDP(60)
+        )
+        findViewById<LinearLayout>(62000+icount).addView(img)
+
+        val textname = TextView(this)
+        textname.text = "감자"
+        textname.gravity= Gravity.CENTER
+        textname.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            changeDP(30)
+        )
+        textname.id=(65000+icount)
+        textname.setBackgroundColor(Color.GRAY)
+        findViewById<LinearLayout>(62000+icount).addView(textname)
+
+        val textdate = TextView(this)
+        textdate.text = icount.toString()
+        textdate.gravity= Gravity.CENTER
+        textdate.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            changeDP(20)
+        )
+        textdate.setBackgroundColor(Color.WHITE)
+        textdate.id=(66000+icount)
+        findViewById<LinearLayout>(62000+icount).addView(textdate)
+
+        val checkbox = CheckBox(this)
+        checkbox.scaleX= 1.5F
+        checkbox.scaleY= 1.5F
+        checkbox.layoutParams = LinearLayout.LayoutParams(
+            changeDP(30), changeDP(30)
+        )
+        checkbox.id=(67000+icount)
+        findViewById<LinearLayout>(63000+icount).addView(checkbox)
+        checkbox.setOnCheckedChangeListener { buttonView, a ->
+            println("checkbox")
+            print(checkbox.id)
+            println(a)
+        }
+    }
+    private fun changeDP(value : Int) : Int{        //int값을 dp값으로 변환
+        var displayMetrics = resources.displayMetrics
+        var dp = Math.round(value * displayMetrics.density)
+        return dp
+    }
+
 }
