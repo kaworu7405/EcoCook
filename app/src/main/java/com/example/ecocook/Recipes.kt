4fdff20673a044fb253d1357f7826a31a6e1b37c
jina0 <1226___snow@naver.com>
2021-10-29 21:20:03 +0900

    DBforFridgeAndRecipes 디비 업데이트되었습니다.

---
+++ app/src/main/java/com/example/ecocook/Recipes.kt
@@ -0,0 +1,9 @@
+package com.example.ecocook
+
+class Recipes {
+    val category : String?=null //양식, 중식, 한식 등..분류
+    val comment : String?=null //음식 설명
+    val ingredients : List<String>?=null //재료
+    val instruction : List<String>?=null //만드는 방법
+    val name : String?=null //음식이름
+}
