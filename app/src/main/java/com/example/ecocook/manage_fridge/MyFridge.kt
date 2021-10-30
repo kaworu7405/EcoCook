a642b45776e4d713474cf34755ec5b8cd2374dc0
Jaehun.Kim <rlawogns0411@naver.com>
2021-10-30 13:33:18 +0900

    style : add comment

--- app/src/main/java/com/example/ecocook/manage_fridge/MyFridge.kt
+++ app/src/main/java/com/example/ecocook/manage_fridge/MyFridge.kt
@@ -14,17 +14,18 @@ import com.example.ecocook.R
 import kotlinx.android.synthetic.main.activity_my_fridge.*
 
 class MyFridge : AppCompatActivity() {
-    var ingredient = 0
-    var icount=0
-    var lcount=0
+    var ingredient = 0      //나의 총 재료 개수
+    var icount=0            //현재 만들어진 재료
+    var lcount=0            //현재 만들어진 줄개수
     var backKeyPressedTime:Long=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_fridge)
         add_btn.setOnClickListener(View.OnClickListener {
-            //startActivity(Intent(this, AddIngredient::class.java))
+            //startActivity(Intent(this, AddIngredient::class.java))    //원래는 여기로 넘어가야댐
             //finish()
-            if(icount==lcount*4){
+            //임시로 추가하는거 보기 위해 해놈
+            if(icount==lcount*4){   //한 줄에 4개씩 보여주기 위함 4개가 이미 있으면 tablerow추가 생성
                 AddTablerow()
                 lcount+=1
             }
@@ -41,9 +42,8 @@ class MyFridge : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         var pref = getSharedPreferences("pref",Context.MODE_PRIVATE)
-        ingredient = pref.getInt("ingredient",0)
-        System.out.println(ingredient)
-        for(i in 0..ingredient-1){
+        ingredient = pref.getInt("ingredient",0)    //ingredient가져오기 값없으면 0을 가져옴
+        for(i in 0..ingredient-1){      //현재 있는 재료들 모두 생성
             if(icount==lcount*4){
                 AddTablerow()
                 lcount+=1
@@ -57,30 +57,24 @@ class MyFridge : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         var pref = getSharedPreferences("pref",Context.MODE_PRIVATE)
-        pref.edit().putInt("ingredient",icount).apply()
+        pref.edit().putInt("ingredient",icount).apply()             //화면종료? 이동시? ingredient저장
     }
     override fun onBackPressed() {
-        /*if(System.currentTimeMillis()-backKeyPressedTime<2000){
-            finishAffinity()
-        }
-        backKeyPressedTime=System.currentTimeMillis()*/
         startActivity(Intent(this, MainMenuActivity::class.java))
         finish()
     }
     fun AddTablerow(){        //한 줄 추가
         val LL= TableRow(this)
         LL.setBackgroundColor(Color.GREEN)
-        //LL.id=id                      //rse에 있는 아이디사용
         LL.id=(50000+lcount)
         table1.addView(LL,TableRow.LayoutParams.MATCH_PARENT, changeDP(110))
-        //(findViewById<LinearLayout>(10000+ingredient).layoutParams as LinearLayout.LayoutParams).weight=1f    weight설정
 
 
     }
     @SuppressLint("ResourceType")       //이건 뭔지 잘 모르겠다, id=(int값)할 때 뜨는 에러때문에 추가
     fun AddLinear(){        //한 줄 추가
-        //val idArray = resources.obtainTypedArray(R.array.id_group_1)      //res에 있는 아이디사용
-        //val id = idArray.getResourceId(ingredient,-1)
+        //val idArray = resources.obtainTypedArray(R.array.id_group_1)      //res에 있는 id.xml 아이디사용  일단은 다른 방법으로 해서 안씀
+        //val id = idArray.getResourceId(ingredient,-1)                     //위와 동일
         val LL= LinearLayout(this)
         LL.setBackgroundColor(Color.GREEN)
         //LL.id=id                      //rse에 있는 아이디사용
@@ -88,18 +82,15 @@ class MyFridge : AppCompatActivity() {
         LL.gravity=Gravity.CENTER
         LL.orientation=LinearLayout.VERTICAL
         findViewById<TableRow>(50000+lcount-1).addView(LL,LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
-        //(findViewById<LinearLayout>(10000+ingredient).layoutParams as LinearLayout.LayoutParams).weight=1f    weight설정
 
 
     }
     @SuppressLint("ResourceType")   //이건 뭔지 잘 모르겠다, id=(int값)할 때 뜨는 에러때문에 추가
     fun AddIngredient(){    //재료 추가
-        //val idArray = resources.obtainTypedArray(R.array.id_group_1)  //rse에 있는 아이디사용
-        //val id = idArray.getResourceId(ingredient,-1)
 
-        val img= ImageView(this)
-        img.setImageResource(R.drawable.testaa)
-        img.scaleType=ImageView.ScaleType.CENTER_CROP
+        val img= ImageView(this)            //이미지
+        img.setImageResource(R.drawable.testaa)     //이미지설정
+        img.scaleType=ImageView.ScaleType.CENTER_CROP       //scaletype설정
         img.id=(20000+icount)
         img.layoutParams = LinearLayout.LayoutParams(
             changeDP(100),
@@ -108,8 +99,8 @@ class MyFridge : AppCompatActivity() {
         findViewById<LinearLayout>(10000+icount).addView(img)
 
         val textname = TextView(this)
-        textname.text = "감자"
-        textname.gravity=Gravity.CENTER
+        textname.text = "감자"                    //재료이름
+        textname.gravity=Gravity.CENTER         //gravity설정
         textname.layoutParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.WRAP_CONTENT,
             changeDP(30)
@@ -118,9 +109,9 @@ class MyFridge : AppCompatActivity() {
         textname.setBackgroundColor(Color.GRAY)
         findViewById<LinearLayout>(10000+icount).addView(textname)
 
-        val textdate = TextView(this)
+        val textdate = TextView(this)           //유통기한 설정
         textdate.text = "D-3"
-        textdate.gravity=Gravity.CENTER
+        textdate.gravity=Gravity.CENTER                 //gravity설정
         textdate.layoutParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.WRAP_CONTENT,
             changeDP(20)
