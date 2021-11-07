00a4ff5dcb09bbf9bd0ffd45c3afdb70f0e2f5c2
JinaHong <73168867+kaworu7405@users.noreply.github.com>
2021-11-08 01:26:12 +0900

    Message 기능 구현

--- app/src/main/java/com/example/ecocook/MessageActivity.kt
+++ app/src/main/java/com/example/ecocook/MessageActivity.kt
@@ -1,14 +1,81 @@
 package com.example.ecocook
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_message.*
+import kotlinx.android.synthetic.main.activity_posting_detail.*
 
-class MessageActivity: AppCompatActivity()  {
+class MessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
 
         val message = intent.getSerializableExtra("message") as Message
+        val db = Firebase.firestore
 
+        val docRef = db.collection("Message").document(message.id.toString())
+        docRef.addSnapshotListener { snapshot, e ->
+            if (e != null) {
+                Log.w("TAG", "Listen failed.", e)
+                return@addSnapshotListener
+            }
+
+            if (snapshot != null && snapshot.exists()) {
+                val obj = snapshot.toObject<Message>()
+                val messageAdapter =
+                    message.message?.let {
+                        obj?.message?.let { it1 ->
+                            MessageContentAdapter(
+                                this, R.layout.message_content_view,
+                                it1
+                            )
+                        }
+                    }
+                messageLogList.adapter = messageAdapter
+            } else {
+                Log.d("TAG", "Current data: null")
+            }
+        }
+        messageSendBtn.setOnClickListener {
+            if (chattingInputText.text.toString().isNotEmpty()) {
+                val user = Firebase.auth.currentUser
+                var arr = ArrayList<Map<String, String>>()
+                val docRef = db.collection("Message").document(message.id.toString())
+                docRef.get()
+                    .addOnSuccessListener { document ->
+                        if (document != null) {
+                            if (document.exists()) {
+                                val obj = document.toObject<Message>()
+                                var chatting = obj?.message
+                                if (chatting != null) {
+                                    for (c in chatting) {
+                                        arr.add(c)
+                                    }
+                                }
+                                if (user != null) {
+                                    arr.add(mapOf(user.uid to chattingInputText.text.toString()))
+                                }
+                                if (obj != null) {
+                                    obj.message = arr
+                                    docRef.set(obj)
+                                }
+
+                            }
+                        }
+                    }
+                chattingInputText.setText("")
+            }
+        }
     }
+
+
 }
--- app/src/main/java/com/example/ecocook/MyMessageActivity.kt
+++ app/src/main/java/com/example/ecocook/MyMessageActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ecocook
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.ktx.auth
@@ -8,6 +9,7 @@ import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_message.*
 import kotlinx.android.synthetic.main.activity_my_message.*
 import kotlinx.android.synthetic.main.activity_my_posting.*
 
@@ -19,9 +21,10 @@ class MyMessageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_my_message)
 
         setMessage()
+        chattingClicked()
     }
 
-    fun setMessage(){
+    fun setMessage() {
         val user = Firebase.auth.currentUser
         if (user != null) {
             val db = Firebase.firestore
@@ -33,7 +36,7 @@ class MyMessageActivity : AppCompatActivity() {
                         for (document in querySnapshot.documents) {
                             val obj = document.toObject<Message>()
                             if (obj != null) {
-                                if(user.uid==obj.user1 || user.uid==obj.user2){
+                                if (user.uid == obj.user1 || user.uid == obj.user2) {
                                     messageList.add(0, obj)
                                 }
                             }
@@ -42,6 +45,16 @@ class MyMessageActivity : AppCompatActivity() {
                         myMessageListView.adapter = messageAdapter
                     }
                 }
+        }
+    }
+
+    fun chattingClicked(){
+        myMessageListView.setOnItemClickListener { adapterView, view, i, l ->
+            val clickedMessage=messageList[i]
+            val myIntent = Intent(this, MessageActivity::class.java)
+            myIntent.putExtra("message", clickedMessage)
+
+            startActivity(myIntent)
         }
     }
 
